d31e295fb24de72310c1cf6f7b925a3800e91830
mac <sohadwd9@gmail.com>
2018-07-15 14:25:41 +0300

    modify drawer in secon screen tac icon

--- example/src/screens/SecondScreenTabIcon/SecondScreenTabIcon.js
+++ example/src/screens/SecondScreenTabIcon/SecondScreenTabIcon.js
@@ -2,6 +2,21 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 
 class FirstScreenTabIcon extends Component {
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
+  onNavigatorEvent = event => {
+    if (event.type === "NavBarButtonPress") {
+      if (event.id === "sideDrawerToggle") {
+        this.props.navigator.toggleDrawer({
+          side: "left"
+        });
+      }
+    }
+  };
+
   render() {
     return (
       <View>
